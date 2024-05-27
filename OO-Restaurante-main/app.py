b835f2cd038a2ac70267e6633220afaff80e7dfc
Matheus-Alechandre <162439808+Matheus-Alechandre@users.noreply.github.com>
2024-05-27 12:10:04 -0300

    Add files via upload

--- OO-Restaurante-main/app.py
+++ OO-Restaurante-main/app.py
@@ -5,6 +5,15 @@ restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
 restaurante_praca.receber_avaliacao('Emy', 2)
 
+restaurante_mexicano = Restaurante('Mexicano', 'Burrito')
+restaurante_mexicano.receber_avaliacao('Gui',5)
+
+restaurante_Larissa = Restaurante('Larrisinha', 'X-Salada')
+restaurante_Larissa.receber_avaliacao('Daniel',4.2)
+restaurante_Larissa.receber_avaliacao('Gaby',3.2)
+
+restaurante_Larissa.alternar_estado()
+
 def main():
     Restaurante.listar_restaurantes()
 
